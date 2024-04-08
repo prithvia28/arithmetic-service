a1b15d9d9bb04c8dc6a59b6fa2e46df419fab549
Prithvi <prithvia28@gmail.com>
2024-04-07 22:13:38 -0400

    unit testing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const {add}= require("./arithmetica")
+
 app.use(cors());
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Arithmetic service - Hello World!");
+  res.send("Arithmetic service - last updated at 10:05PM");
 });
 
 app.get("/add/:n/:m", (req, res) => {
